7afdbe0a9586ad52902cafbad0a7fc4557558877
Gabriel Vautrin <gvautrin@serial.ch>
2020-06-18 11:55:19 +0200

    authProvider

--- src/App.js
+++ src/App.js
@@ -6,10 +6,15 @@ import { PostList, PostEdit, PostCreate } from './posts'
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import Dashboard from './Dashboard'
+import authProvider from './authProvider';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
+  <Admin 
+    dashboard={Dashboard} 
+    dataProvider={dataProvider}
+    authProvider={authProvider}
+  >
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
     <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
